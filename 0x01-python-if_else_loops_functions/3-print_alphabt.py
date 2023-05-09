5df0ee17ba08ca7166315adecaa626f7355ea44e
warimap <warimapharium@.com>
2023-05-09 09:06:44 -0700

    Task 3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for char in range(97, 123):
+    if char == 101 or char == 113:
+        continue
+    print("{:c}".format(char), end='')
