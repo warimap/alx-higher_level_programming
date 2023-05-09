145ff6c5606231a6d00d3aa00b38d897eeef438a
warimap <warimapharium@.com>
2023-05-09 09:21:02 -0700

    Task 13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,39 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
 /**
- * insert_node -  inserts a number into a sorted singly linked list
- * @head: singly list head
- * @number: number to insert
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to the head of the list
+ * @number: number to insert into the list
+ *
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *current;
+    listint_t *new_node, *current_node, *prev_node;
 
-	current = *head;
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+    new_node->n = number;
+    new_node->next = NULL;
 
-	new->n = number;
+    if (*head == NULL || (*head)->n >= number) {
+        new_node->next = *head;
+        *head = new_node;
+    } else {
+        current_node = *head;
+        while (current_node != NULL && current_node->n < number) {
+            prev_node = current_node;
+            current_node = current_node->next;
+        }
+        prev_node->next = new_node;
+        new_node->next = current_node;
+    }
 
-	if (*head == NULL)
-		*head = new;
-	else
-	{
-		while (current->next != NULL)
-		{
-			if (current->next->n > number)
-				break;
-
-			current = current->next;
-		}
-		new->next = current->next;
-		current->next = new;
-	}
-
-	return (new);
+    return (new_node);
 }
