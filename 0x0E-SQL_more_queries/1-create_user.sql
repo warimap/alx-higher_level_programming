61c58a4ade0d408c38e17924879999abf5ae466e
Faith Warima Ng'endo <warimapharium@gmail.com>
2023-07-19 17:40:42 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
--- creates the MySQL server user user_0d_1 with all privileges
--- password should be set to user_0d_1_pwd and 
--- should not fail if user already exists
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- create a user and set a password of the user
+-- give it all the permission
+-- flush privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
