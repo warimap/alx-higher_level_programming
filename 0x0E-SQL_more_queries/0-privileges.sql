2faca97769d45dce53d4b8217922b02ca763eb7d
warimap <warimapharium@.com>
2023-07-19 02:18:55 -0700

    Task 0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2.
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
