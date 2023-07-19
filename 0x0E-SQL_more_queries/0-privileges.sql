1f9354efffc2a057d6ea126729a3cf3260069521
warimap <warimapharium@.com>
2023-07-19 07:10:12 -0700

    Task 0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- Script that lists all privileges of the MySQL users
--- Query to list all privileges (GRANT) of the MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query
+FROM mysql.user
+WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost'
+ORDER BY user;
