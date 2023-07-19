f9d3f9cb9da951c513f9e5e9665b435736f51309
Faith Warima Ng'endo <warimapharium@gmail.com>
2023-07-19 17:15:05 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Script that lists all privileges of the MySQL users
-SELECT CONCAT('SHOW GRANTS FOR ''', user, '''@''localhost'';') AS query FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
-
+-- Lists all privileges of the users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
