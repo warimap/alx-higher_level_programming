c63031208791eba679ded27c79d14ecd7700e770
warimap <warimapharium@.com>
2023-07-19 06:41:35 -0700

    Task 0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
+-- For user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- For user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
