1df310a63e6689a0f9d7837dc09cdf0544719849
Your Name <warimapharium@gmail.com>
2024-01-07 00:52:54 -0800

    Task 4 updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+Script that fetches https://intranet.hbtn.io/status
+You must use the package requests.
+"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    url = 'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
